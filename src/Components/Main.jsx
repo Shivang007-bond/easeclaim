1bad00f5644a5ba12983bb4ce3026840fb9363cd
Shivang Tripathi <77275400+Shivang007-bond@users.noreply.github.com>
2024-02-27 12:49:57 +0530

    commit check

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Main = () => {
   return (
     <div>
-      <div className="h-screen bg-[#2F3045] rounded-b-3xl flex flex-col justify-center align-middle">
-        <div className="w-11/12 h-5/6 m-auto flex justify-between mt-16 bg-[#FFFFFF] p-4 rounded-xl">
-          <span className="font-semibold text-2xl">Plans Description</span>
-          <span className="text-md">Basic</span>
-          <span className="text-md">Standard</span>
-          <span className="text-md">Premium</span>
+      <div className="h-screen bg-[#2F3045] rounded-b-3xl flex flex-col justify-center align-middle p-8">
+        <div className="w-11/12 h-5/6 m-auto flex justify-between bg-[#FFFFFF] p-4 rounded-xl">
+          <div className="font-semibold text-2xl">Plans Description</div>
+          <div className="text-md">Basic</div>
+          <div className="text-md">Standard</div>
+          <div className="text-md">Premium</div>
         </div>
         <button className="bg-white w-24 m-auto text-[#476BC7] rounded-md p-2">
           Get Started
